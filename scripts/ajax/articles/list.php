39d994ed2961d7b59a5592f65628fee709cc4639
Razvan <tangent.jotey@gmail.com>
2015-02-21 19:45:55 +0100

    Update and rename scripts/ajax/articles to scripts/ajax/articles/list.php

---
+++ scripts/ajax/articles/list.php
@@ -0,0 +1,9 @@
+<?php
+print("
+	<article class='list'>	
+		<header class='tooltip' tooltip='Published by <u>".$fetch[2]."</u>'><h2>".$fetch[1]."</h2></header>
+		<p>		".$fetch[3]."</p>
+		<footer class='tooltip' tooltip='Notice published on <u>".$fetch[4]."</u>'>".$fetch[4]."</footer>
+	</article>");
+
+?>
